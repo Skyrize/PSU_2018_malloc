e66688c5765095768336d3edf0f118746adaabd8
Leos Julien <leos.julien@gmail.com>
2019-01-25 16:17:44 +0100

    just add some debug write

--- malloc.c
+++ malloc.c
@@ -20,6 +20,12 @@ void	my_putchar(char c)
 	write(1, &c, 1);
 }
 
+void    my_putstr(char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+        my_putchar(str[i]);
+}
+
 int	my_putnbr(int nb)
 {
 	if (nb < 0) {
@@ -65,6 +71,9 @@ void    create_info_block(void * *ptr, size_t size)
 
     info_block->is_free = 0; 
     info_block->size = size;
+    my_putstr("MALLOC - CREATE BLOCK OF : ");
+    my_putnbr(size);
+    my_putchar('\n');
 }
 
 void    *browse_alloc(size_t size)
@@ -75,6 +84,9 @@ void    *browse_alloc(size_t size)
     int save_block_size = 0;
 
     while (((info_t *)current)->is_free != 2) {
+        my_putstr("MALLOC - NEXT BLOCK IS : ");
+        my_putnbr(((info_t *)current)->size);
+        my_putchar('\n');
         if (((info_t *)current)->is_free == 1 &&
         size <= ((info_t *)current)->size) {
             save_block_size = ((info_t *)current)->size;
@@ -83,9 +95,13 @@ void    *browse_alloc(size_t size)
                 current += info_size + ((info_t *)current)->size;
                 create_info_block(&current, save_block_size - size - info_size);
                 ((info_t *)current)->is_free = 1;
+                my_putstr("MALLOC - RETURN AFTER SPLIT\n\n");
+                // show_alloc_mem();
+                my_putstr("\n\n");
                 return (current - size);
             }
             ((info_t *)current)->is_free = 0;
+            my_putstr("MALLOC - RETURN AFTER REALLOC\n\n");
             return (current + info_size);
         }
         current += info_size + ((info_t *)current)->size;
@@ -102,11 +118,15 @@ void    *browse_alloc(size_t size)
     create_info_block(&current, new_block_size);
     ((info_t *)current)->is_free = 2;
 
+    my_putstr("MALLOC - RETURN AFTER CREATE\n\n");
     return (current - size);
 }
 
 void    *malloc(size_t size)
 {
+    my_putstr("MALLOC - SIZE OF : ");
+    my_putnbr(size);
+    my_putchar('\n');
     if (size == 0)
         return (NULL);
     if (!head_ptr) {
@@ -127,32 +147,32 @@ void     free_shrink(void *ptr_free)
     while (((info_t *)current)->is_free != 2) {
         if (((info_t *)current)->is_free == 0) {
             ((info_t *)current)->is_free = 1;
-            write(1, "FREE - DO NOT SHRINK\n", 21);
+            // write(1, "FREE - DO NOT SHRINK\n", 21);
             return;
         }
         current += info_size + ((info_t *)current)->size;
     }
     nb_shrink = (current - ptr_free + ((info_t *)current)->size + info_size) / getpagesize();
     if (nb_shrink > 0) {
-        write(1, "FREE - SHRINK : ", 16);
-        my_putnbr(nb_shrink);
-        write(1, "\n", 1);
+        // write(1, "FREE - SHRINK : ", 16);
+        // my_putnbr(nb_shrink);
+        // write(1, "\n", 1);
 
         sbrk(-nb_shrink);
     }
 
-    write(1, "FREE - PTR FREE : ", 18);
-    my_putnbr(((info_t *)ptr_free)->size);
-    write(1, "\n", 1);
-    write(1, "FREE - ADD TO LAST : ", 21);
-    my_putnbr(((info_t *)current)->size + info_size - (getpagesize() * nb_shrink));
-    write(1, "\n", 1);
+    // write(1, "FREE - PTR FREE : ", 18);
+    // my_putnbr(((info_t *)ptr_free)->size);
+    // write(1, "\n", 1);
+    // write(1, "FREE - ADD TO LAST : ", 21);
+    // my_putnbr(((info_t *)current)->size + info_size - (getpagesize() * nb_shrink));
+    // write(1, "\n", 1);
 
     ((info_t *)ptr_free)->size += ((info_t *)current)->size + info_size - (getpagesize() * nb_shrink);
     
-    write(1, "FREE - PTR FREE AFTER : ", 24);
-    my_putnbr(((info_t *)ptr_free)->size);
-    write(1, "\n", 1);
+    // write(1, "FREE - PTR FREE AFTER : ", 24);
+    // my_putnbr(((info_t *)ptr_free)->size);
+    // write(1, "\n", 1);
 
     ((info_t *)ptr_free)->is_free = 2;
     if (((info_t *)ptr_free)->size == -info_size)
@@ -165,8 +185,10 @@ void    free(void *ptr)
 
     while (((info_t *)current)->is_free != 2) {
         if (current + info_size == ptr) {
-            // write(1, "FREE\n", 5);
+            my_putstr("FREE\n\n");
             free_shrink(current);
+            // show_alloc_mem();
+            my_putstr("\n\n");
             return;
         }
         current += info_size + ((info_t *)current)->size;
